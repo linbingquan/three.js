5e655921d686b6065561beaf12f62a36a3d2f709
linbingquan <695601626@qq.com>
2023-09-05 07:39:02 +0800

    Editor: Suport window-controls-overlay feature.

---
+++ editor/js/Menubar.AppRegion.js
@@ -0,0 +1,12 @@
+import { UIPanel } from './libs/ui.js';
+
+function MenuAppRegion() {
+
+	const container = new UIPanel();
+	container.setClass( 'menu app-region' );
+
+	return container;
+
+}
+
+export { MenuAppRegion };
--- editor/js/Menubar.js
+++ editor/js/Menubar.js
@@ -5,6 +5,7 @@ import { MenubarEdit } from './Menubar.Edit.js';
 import { MenubarFile } from './Menubar.File.js';
 import { MenubarView } from './Menubar.View.js';
 import { MenubarHelp } from './Menubar.Help.js';
+import { MenuAppRegion } from './Menubar.AppRegion.js';
 import { MenubarStatus } from './Menubar.Status.js';
 
 function Menubar( editor ) {
@@ -17,6 +18,8 @@ function Menubar( editor ) {
 	container.add( new MenubarAdd( editor ) );
 	container.add( new MenubarView( editor ) );
 	container.add( new MenubarHelp( editor ) );
+
+	container.add( new MenuAppRegion( editor ) );
 
 	container.add( new MenubarStatus( editor ) );
 
